93570a083cbfe28e0b859f0cbe8ba4b53b771a52
CheolSeung <110644789+CheolSeung@users.noreply.github.com>
2022-12-22 14:47:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const router = require("./router/router");
 
+const session = require("express-session");  // 세션기능
+const mysql_session = require("express-mysql-session");   // 세션이 저장되는 영역(mysql)
+
 
 // (1) 경로처리를 하는 모듈 : path
 // 여러 환경 때문에 경로를 단순히 문자열로 접근하면
@@ -13,6 +16,29 @@ const path = require("path");
 // 데이터를 주고받을 때 필수적으로 등록할 것  npm install cors
 const cors = require("cors");
 
+
+// mysql 정보를 담고있는 변수
+let conn = {
+    host : "127.0.0.1",
+    user : "root",
+    password : "zkddl9197@",
+    port : "3306",
+    database : "healthcare_db"
+}
+
+
+// 인증절차
+let conn_session = new mysql_session(conn)
+
+// 세션 기능 등록 (저장 위치 : mysql)
+app.use(session ({
+    secret : "Smart",    // 비밀키(의미없음)
+    resave : false,           // 저장할건지 말건지 
+    saveUninitialized : true, // 서버 실행시마다 초기화여부
+    store : conn_session      // 저장 장소
+}))
+
+
 // 이 폴더 안에 있는 static 파일을 사용할게요!
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -23,4 +49,4 @@ app.use(cors());
 app.use(express.json());
 
 app.use(router);
-app.listen(3001);
+app.listen(3001,()=>{console.log('port 3001')});
